a6e47bdc11afc9fd7ff45775b8fe8b2534088e80
Carlos Alba <soradev4@gmail.com>
2019-06-25 23:32:06 -0700

    change

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
-package main 
+package main
+
 import (
-  // "net/http"
-  "flag"
-  "fmt"
+	// "net/http"
+	"flag"
+	"fmt"
 )
 
-func main(){
-  v := flag.String("target", "", "target to run DoS Attack on")
-  fmt.Println(v)
-  flag.Parse()
-  fmt.Println(flag.Args())
+func main() {
+	v := flag.String("target", "", "target to run DoS Attack on")
+	fmt.Println(v)
+	flag.Parse()
+	fmt.Println(flag.Args())
 }
