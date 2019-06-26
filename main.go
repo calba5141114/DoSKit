43d86bb1a4f6064e2633029526efaffa65060b77
Carlos Alba <soradev4@gmail.com>
2019-06-25 23:53:33 -0700

    comment

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ var target = flag.String("target", "", "target to run DoS Attack on")
 func main() {
   // channel for accepting responses and passing them through to the pipeline
 	resChannel := make(chan interface{})
-	flag.Parse()
+  flag.Parse()
+  // checking for value in required flags
 	if *target == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
