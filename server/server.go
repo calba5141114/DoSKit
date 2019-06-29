22046a5bf229f1cf8c5f71394164f55479b7b863
Carlos Alba <soradev4@gmail.com>
2019-06-29 13:58:37 -0700

    log

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"log"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -13,4 +14,6 @@ func attackHandler(w http.ResponseWriter, r *http.Request){
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/DosAttack", attackHandler ).Methods("POST")
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
