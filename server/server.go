b7de8cff49ef6f4e98a0c778e2b26a8ccdca4e6b
Carlos Alba <soradev4@gmail.com>
2019-06-29 14:03:38 -0700

    fmt and name

--- server/server.go
+++ server/server.go
@@ -3,25 +3,24 @@ package server
 import (
 	"fmt"
 	"log"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func attackHandler(w http.ResponseWriter, r *http.Request) {
 
-func attackHandler(w http.ResponseWriter, r *http.Request){
-
-}	
-
-func indexHandler(w http.ResponseWriter, r *http.Request){
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "application/html; charset=UTF-8")
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/html; charset=UTF-8")
+	fmt.Fprintf(w, "This is DosKit Server: %s\n", r.URL.Path)
+}
 
-func main(){
+func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/DosAttack", attackHandler ).Methods("POST")
+	r.HandleFunc("/DosAttack", attackHandler).Methods("POST")
 	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
