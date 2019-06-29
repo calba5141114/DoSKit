7babbcaa3703bbfd95772ec6a73f7f889bbed523
Carlos Alba <soradev4@gmail.com>
2019-06-29 14:00:50 -0700

    json

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"log"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -11,9 +12,18 @@ func attackHandler(w http.ResponseWriter, r *http.Request){
 
 }	
 
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(<-listingChannel)
+
+}
+
+
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/DosAttack", attackHandler ).Methods("POST")
+	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
